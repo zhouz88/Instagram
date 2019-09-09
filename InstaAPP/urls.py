bf018306d96b2e514f79057938b5ebbebf400afd
kerwinzhou <zhouz88@uw.edu>
2019-09-08 17:17:54 -0700

    Display static page

--- InstaAPP/urls.py
+++ InstaAPP/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from InstaAPP.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('InstaAPP/', include('InstaAPP.urls')),
+    path('', HelloWorld.as_view(), name = 'home'),
+
 ]
---
+++ InstaAPP/views.py
@@ -0,0 +1,6 @@
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+class HelloWorld(TemplateView):
+    template_name = 'test.html'
