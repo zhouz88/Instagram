14ac7e699e8e5f1fa6a6a94109478d3861fc85a4
kerwinzhou <zhouz88@uw.edu>
2019-09-10 10:39:13 -0700

    CRUD

--- InstaAPP/views.py
+++ InstaAPP/views.py
@@ -1,6 +1,33 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from InstaAPP.models import Post
 
 # Create your views here.
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ('title', 'image') #create models, what kind field do you want users to send?
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title'] 
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("postings")
