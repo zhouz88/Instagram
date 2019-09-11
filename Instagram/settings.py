f7b11eff4549995a5033bf1a1603923a6f2bc170
kerwinzhou <zhouz88@uw.edu>
2019-09-11 14:46:17 -0700

    Prep for deployment

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qw%j3-o4m^dyb9r(i6sgpnp(+1bvk$76@*f*g=)-hbk7i^e1x%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] #all the hosts can visit
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'InstaAPP',
     'imagekit',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
